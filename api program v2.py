628976bbf0af4813e984c11e23dd1eddd4165075
RobertSchuldt <45829365+RobertSchuldt@users.noreply.github.com>
2019-06-03 10:36:50 -0500

    Update api program v2.py

--- api program v2.py
+++ api program v2.py
@@ -16,7 +16,7 @@ ts = datetime.datetime.now().isoformat()
 
 white = "B27001A_008E,B27001A_009E,B27001A_010E,B27001A_011E,B27001A_012E,B27001A_013E,B27001A_014E,B27001A_015E,B27001A_016E"
 black = "B27001B_008E,B27001B_009E,B27001B_010E,B27001B_011E,B27001B_012E,B27001B_013E,B27001B_014E,B27001B_015E,B27001B_016E"
-
+state = "01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50"
 
  #Creating a list of the actual variable names that we are interested in 
 
@@ -24,7 +24,7 @@ def acs_pull(word):
     ''' Identifying the variable I am interested in regarding African American 
         insurance rates this function grabs from a specified year and appends to an existing dataframe '''
 
-    api = "https://api.census.gov/data/" + str(word)+ "/acs/acs1?get=NAME,"+str(white)+","+str(black)+"&for=county:*&in=state:05,48,40,20,29,47,28"
+    api = "https://api.census.gov/data/" + str(word)+ "/acs/acs1?get=NAME,"+str(white)+","+str(black)+"&for=state:*&key=****"
 
     print('Requesting data from the American Communities Survey API')
     
@@ -37,9 +37,9 @@ def acs_pull(word):
     
     content = acs_data.json()
     print("Data converted to JSON formatting from ACS")
-    headers= ['County_Name','Total_White_19_25' ,'Total_White_Insur_19_25','Total_White_UnInsur_19_25','Total_White_26_34' ,'Total_White_Insur_26_34','Total_White_UnInsur_26_34',
+    headers= [ 'State','Total_White_19_25' ,'Total_White_Insur_19_25','Total_White_UnInsur_19_25','Total_White_26_34' ,'Total_White_Insur_26_34','Total_White_UnInsur_26_34',
               'Total_White_35_44' ,'Total_White_Insur_35_44','Total_White_UnInsur_35_44', 'Total_AA_19_25' ,'Total_AA_Insur_19_25','Total_AA_UnInsur_19_25','Total_AA_26_34' ,
-              'Total_AA_Insur_26_34','Total_AA_UnInsur_26_34','Total_AA_35_44' ,'Total_AA_Insur_35_44','Total_AA_UnInsur_35_44', 'fips_cnty', 'state']
+              'Total_AA_Insur_26_34','Total_AA_UnInsur_26_34','Total_AA_35_44' ,'Total_AA_Insur_35_44','Total_AA_UnInsur_35_44', 'state']
 
     #I am adding the list of headers that will actually define what the different variables are
 
